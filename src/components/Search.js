2ef80572db09c59d8e6569434cbfc328c5e87462
geepara <gpara1697@gmail.com>
2021-10-19 12:57:33 -0400

    search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,8 @@
-function Search() {
-  return <>Search</>;
+import SearchBar from "material-ui-search-bar";
+
+function Search(props) {
+  const { query, updateQuery } = props;
+  return <SearchBar value={query} onChange={updateQuery} />;
 }
 
 export default Search;
--- src/pages/DisplayNotes.js
+++ src/pages/DisplayNotes.js
@@ -2,6 +2,7 @@ import { List, Fab, withStyles } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 import Note from "../components/Note";
 import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import Search from "../components/Search";
 
 const styles = {
@@ -12,22 +13,48 @@ const styles = {
   },
 };
 
-function DisplayNotes(props) {
-  const { notes, deleteNote, classes } = props;
-  return (
-    <>
-      <List>
-        {notes.map((note, index) => {
-          return <Note note={note} key={index} deleteNote={deleteNote} />;
-        })}
-      </List>
-      <Link to="/add">
-        <Fab aria-label={"Add"} className={classes.fab}>
-          <Add />
-        </Fab>
-      </Link>
-    </>
-  );
+class DisplayNotes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    };
+  }
+
+  updateQuery = (newValue) => {
+    this.setState({
+      query: newValue,
+    });
+  };
+
+  includes = (note) => {
+    const query = this.state.query.toLowerCase().trim();
+    return (
+      note.title.toLowerCase().includes(query) ||
+      note.text.toLowerCase().includes(query)
+    );
+  };
+
+  render() {
+    const { notes, deleteNote, classes } = this.props;
+    return (
+      <>
+        <Search>
+          query={this.state.query}; updateQuery={this.updateQuery};
+        </Search>
+        <List>
+          {notes.map((note, index) => {
+            return <Note note={note} key={index} deleteNote={deleteNote} />;
+          })}
+        </List>
+        <Link to="/add">
+          <Fab aria-label={"Add"} className={classes.fab}>
+            <Add />
+          </Fab>
+        </Link>
+      </>
+    );
+  }
 }
 
 export default withStyles(styles)(DisplayNotes);
